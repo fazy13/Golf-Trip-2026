dae3a86800cbf0c9c91f011c024e200db0d43aaf
fazy13 <fazy13@gmail.com>
2026-04-04 10:52:06 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GolfTripApp from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GolfTripApp />
+  </React.StrictMode>
+)
